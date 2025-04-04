573109df0093dd0fb44ace00978badfca5902b42
Santiago <santigs211@gmail.com>
2025-04-03 23:43:55 -0300

    feat: adding new exercise

---
+++ Java Arraylist/Solution.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        
+        Scanner sc = new Scanner(System.in);
+        int numberOfArrays = sc.nextInt();
+        
+        
+        
+        
+        
+        
+        
+        List<int[]> arrayNumbers = new ArrayList<>();
+        
+        for(int i = 0; i<= numberOfArrays;i++){
+            arrayNumbers.add(i, );
+        }
+        
+        
+        sc.close();
+    }
+}
